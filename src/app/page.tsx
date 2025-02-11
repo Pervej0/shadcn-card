353fb80e8c0f2db4924cbce26d467f52740e1f29
pervej0 <mdparvez222khan@gmail.com>
2025-02-11 22:58:07 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import TrendingTabs from "@/shared/ui/TrendingTabs/TrendingTabs";
 export default function Home() {
   return (
     <>
+      <h1 className="text-2xl text-center py-5">
+        This Is new Card Page Section
+      </h1>
       <TrendingTabs />
     </>
   );
